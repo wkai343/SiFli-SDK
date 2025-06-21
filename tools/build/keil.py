7f1e95ece409c5ddb1d067afc100d3e40fa1abde
Xiaoming Xi <xiaomingxi@sifli.com>
2025-06-21 22:43:05 +0800

    [bug][build] Fix keil project generation fail
    ext-redmine: #id, REDMINE-id
    
    [Description in detail]
    No need to decode as name has been already in `str` type
    
    Change-Id: I5883550093c2dc040e6625dd25e8c4a136993dee

--- tools/build/keil.py
+++ tools/build/keil.py
@@ -92,12 +92,12 @@ def MDK4AddGroupForFN(ProjectFiles, parent, name, filename, project_path):
     if ProjectFiles.count(obj_name):
         name = basename + '_' + name
     ProjectFiles.append(obj_name)
-    file_name.text = name.decode(fs_encoding)
+    file_name.text = name
     file_type = SubElement(file, 'FileType')
     file_type.text = '%d' % _get_filetype(name)
     file_path = SubElement(file, 'FilePath')
 
-    file_path.text = path.decode(fs_encoding)
+    file_path.text = path
 
 def MDK4AddLibToGroup(ProjectFiles, group, name, filename, project_path):
     name = os.path.basename(filename)
